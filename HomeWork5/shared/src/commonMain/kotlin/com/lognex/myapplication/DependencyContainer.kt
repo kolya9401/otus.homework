8adec2eb43af6b151624723068a2c05258fba820
Николай Жабоедов <nzhaboedov@moysklad.ru>
2025-06-21 11:16:28 +0300

    Реализовал метод создания АПИ

--- HomeWork5/shared/src/commonMain/kotlin/com/lognex/myapplication/DependencyContainer.kt
+++ HomeWork5/shared/src/commonMain/kotlin/com/lognex/myapplication/DependencyContainer.kt
@@ -1,6 +1,18 @@
 package com.lognex.myapplication
 
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.openapitools.client.apis.CharactersApi
+import org.openapitools.client.infrastructure.ApiClient
+
 class DependencyContainer {
-    fun makeAPI() {
+    fun makeAPI(): CharactersApi {
+        return CharactersApi(
+            baseUrl = ApiClient.BASE_URL,
+            httpClient = HttpClient(Darwin)
+        )
     }
 }
